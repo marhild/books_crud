21f96ea1b9ca3e6f82831787db3793f1886dc180
Maria Hildebrandt <hildebrandt.maria@googlemail.com>
2019-03-26 11:11:37 +0100

    first commit

---
+++ src/main/java/com/example/springbookscrudapp/SpringBooksCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbookscrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooksCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooksCrudAppApplication.class, args);
+	}
+
+}
